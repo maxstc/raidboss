6a857d7e6aa633188c5929ff65e925715a52acfa
maxstc <a@a.a>
2023-04-04 15:34:09 +0000

    adding dynamic sprite drawing

--- client/index.js
+++ client/index.js
@@ -1,9 +1,21 @@
+const DEBUG_IMAGE_RECT_COLOR = "green";
+const DEBUG_COLLISION_RECT_COLOR = "red";
+
 let canvas;
 let ctx;
+let camX;
+let camY;
+
+let midX = 0;
+let midY = 0;
+
+let sprites = [];
 
 function resizeCanvas() {
     canvas.setAttribute("width", window.innerWidth);
     canvas.setAttribute("height", window.innerHeight);
+    midX = window.innerWidth / 2;
+    midY = window.innerHeight / 2;
 }
 
 window.onload = () => {
@@ -21,4 +33,29 @@ window.onload = () => {
 
 window.onresize = () => {
     resizeCanvas();
+}
+
+function render() {
+    for (let i = 0; i < sprites.length; i++) {
+        let s = sprites[i];
+        ctx.drawImage(
+            s.image, 
+            s.x - camX + midX - s.halfWidth,
+            s.y - camY + midY - s.halfHeight,
+            s.width,
+            s.height
+        );
+        //todo put this if outside the for?
+        if (debug) {
+            if (s.image === null) {
+
+            }
+            else { 
+                ctx.strokeStyle = DEBUG_IMAGE_RECT_COLOR;
+                ctx.strokeRect(s.x - (s.image.width / 2), s.y - (s.image.height / 2), s.image.width, s.image.height);
+            }
+            ctx.strokeStyle = DEBUG_COLLISION_RECT_COLOR;
+            ctx.strokeRect(s.x - s.halfWidth, s.y - s.halfHeight, s.width, s.height);
+        }
+    }
 }
