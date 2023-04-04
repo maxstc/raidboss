bd7008b2338df832df4872fe2555c817b0956c75
maxstc <a@a.a>
2023-04-04 14:48:28 +0000

    client displays image from server

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
 let canvas;
+let ctx;
 
 function resizeCanvas() {
     canvas.setAttribute("width", window.innerWidth);
     canvas.setAttribute("height", window.innerHeight);
-    console.log(canvas.getAttribute("width"));
 }
 
 window.onload = () => {
     canvas = document.getElementsByTagName("canvas")[0];
+    ctx = canvas.getContext("2d");
     resizeCanvas();
+
+    myimage = new Image();
+    myimage.onload = () => {
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(myimage, 100, 100, myimage.width*8, myimage.height*8);
+    }
+    myimage.src = "client/images/monster.png";
 }
 
 window.onresize = () => {
