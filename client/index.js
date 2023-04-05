f387bd6840a5a5241009908a574a5cd4dd446dc3
maxstc <a@a.a>
2023-04-05 11:21:35 +0000

    add invisible sprite

--- client/index.js
+++ client/index.js
@@ -28,6 +28,7 @@ window.onload = () => {
     myimage = new Image();
     myimage.onload = () => {
         sprites.push({x: 100, y: 100, width: 200, height: 200, image: myimage, halfWidth: 100, halfHeight: 100, halfImageWidth: myimage.width / 2, halfImageHeight: myimage.height / 2});
+        sprites.push({x: 200, y: 200, width: 50, height: 50, image: null, halfWidth: 25, halfHeight: 25});
     }
     myimage.src = "client/images/monster.png";
 }
@@ -41,21 +42,23 @@ setInterval(render, 50);
 function render() {
     for (let i = 0; i < sprites.length; i++) {
         let s = sprites[i];
-        ctx.drawImage(
-            s.image, 
-            s.x - camX + midX - s.halfImageWidth,
-            s.y - camY + midY - s.halfImageHeight,
-            s.image.width,
-            s.image.height
-        );
+        if (s.image != null) {
+            ctx.drawImage(
+                s.image, 
+                s.x - camX + midX - s.halfImageWidth,
+                s.y - camY + midY - s.halfImageHeight,
+                s.image.width,
+                s.image.height
+            );
+        }
         //todo put this if outside the for?
         if (debug) {
             if (s.image === null) {
-                console.log("tell max to put a circle here");
+                ctx.strokeStyle = DEBUG_IMAGE_RECT_COLOR;
+                ctx.strokeRect(s.x - 10 + midX, s.y - 10 + midY, 20, 20);
             }
             else { 
                 ctx.strokeStyle = DEBUG_IMAGE_RECT_COLOR;
-                console.log(s.image.width);
                 ctx.strokeRect(s.x - s.halfImageWidth + midX, s.y - s.halfImageHeight + midY, s.image.width, s.image.height);
             }
             ctx.strokeStyle = DEBUG_COLLISION_RECT_COLOR;
